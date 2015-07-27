72c3a6cdb6d7d3cc99b4d9af5e3ef8cb68fb8486
supershdow <benjihofing@gmail.com>
2015-07-26 20:45:44 -0400

    Added health counter and lose condition

--- app.py
+++ app.py
@@ -5,29 +5,38 @@ app=Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def root():
+    global ch
+    ch=10
     tool=[]
     for i in range(3):
         tool.append(util.tool())
     #if random.random()<.1:
         #return render_template('final.html')
     #else:
-    return render_template('main.html',story='Dave the superstar sloth has to save the princess in a forest.',instruction='What tool do you pick up?',tools=tool, ct='')
+    return render_template('main.html',story='Dave the superstar sloth has to save the princess in a forest.',instruction='What tool do you pick up?',tools=tool, health=util.get_health(10))
 
-@app.route('/event',methods=['POST'])
+@app.route('/event',methods=['POST','GET'])
 def event():
     global ct
     ct=util.get(request.form['tool'])
     if request.method=="POST":
-        return render_template('main.html',story=util.newEvent(),instruction='Use your tool or flee',ct=util.get(ct),opt='yes')
+        return render_template('main.html',story=util.newEvent(),instruction='Use your tool or flee',ct=util.get(ct),opt='yes',health=ch)
+    elif request.method=="GET":
+        return 'GET'
     else:
         return 'yo'
 
-@app.route('/newtool')
+@app.route('/newtool', methods=['POST','GET'])
 def newtool():
+    if request.form['choice']=='Flee':
+        global ch
+        ch-=1
+        if ch<=0:
+            return render_template('lose.html')
     tool=[]
     for i in range(3):
         tool.append(util.tool())
-    return render_template('main.html',story='Pick a new tool',tools=tool,ct=util.get(ct))
+    return render_template('main.html',story='Pick a new tool',tools=tool,ct=util.get(ct),health=ch)
 
 if __name__=='__main__':
     app.debug=True
--- app.py~
+++ app.py~
@@ -11,13 +11,23 @@ def root():
     #if random.random()<.1:
         #return render_template('final.html')
     #else:
-    if request.method=="GET":
-        return render_template('main.html',story='Dave the superstar sloth has to save the princess in a forest.',instruction='What tool do you pick up?',tools=tool, ct='')
-    elif request.method=="POST":
-        return render_template('main.html',story=util.newEvent(),instruction='Use your tool or flee',ct=util.get(request.form['tool']))
+    return render_template('main.html',story='Dave the superstar sloth has to save the princess in a forest.',instruction='What tool do you pick up?',tools=tool, ct='')
+
+@app.route('/event',methods=['POST'])
+def event():
+    global ct
+    ct=util.get(request.form['tool'])
+    if request.method=="POST":
+        return render_template('main.html',story=util.newEvent(),instruction='Use your tool or flee',ct=util.get(ct),opt='yes')
     else:
         return 'yo'
 
+@app.route('/newtool')
+def newtool():
+    tool=[]
+    for i in range(3):
+        tool.append(util.tool())
+    return render_template('main.html',story='Pick a new tool',tools=tool,ct=util.get(ct))
 
 if __name__=='__main__':
     app.debug=True
