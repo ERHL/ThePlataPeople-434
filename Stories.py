580ea69c158820c327ad20708ffd62e91056d5ac
lIIlIIlIIl <ehainesleblanc@millenniumbrooklynhs.org>
2015-07-27 10:50:24 -0400

    STREES

---
+++ Stories.py
@@ -0,0 +1,11 @@
+EVENTS = [ 'A creepy old man starts following you','You see an abandoned car in the distance', 'You see a town in the distace. You must be getting closer!', 'In the town you see a {{store1}}, a {{store2}}, and a {{store3}}', 'You approach the {{store}}, the door appears to be {{doorthings}}', 'You smash the door a few times, it {{opens / doesnt open}}', 'Your {{tool}} breaks!', 'After searching you found a {{thing}}', 'You continue onwards, leaving the town behind you', "You've reached the {{objective}}!", 'It starts to {{weather}}', 'A Zombie approaches you',] 
+
+#WeatherNote: While weather is going on, new decisions should pop up alongside normals ones. Take shelter and whatnot. If you ignore them there are penalties
+#Weather = Rain, Thunder, Snow, Hail, Fog up
+
+#StoreNote: After visiting 2 stores, you leave town no matter what. (Even if you couldnt get into the store)
+#Stores = Weapons shop, Grocery store, Church, House, Bakery, Police station <-- Very unlikely (Should be super strong though)
+
+#Door things = locked (2.5,0,-1) / jammed (1.5,0,-1) / stuck (1,0,-1) / gone (-1,0,-1) / heavily armored (5,0,-1)
+
+
