8562ea86c693818f407ab82168034f74ddf8f26c
shipmate <mjrabinowitz01@gmail.com>
2015-07-29 11:45:49 -0400

    There. Re-fixed bugs. I HATE THE GODDAMN AUTO-MERGE

--- app.py
+++ app.py
@@ -72,6 +72,8 @@ def newtool():
             ch-=dif
     elif request.form['choice']=='Use tool':
         if act.values()[0][2]==-1:#If the event has no scavenging (is a fight), and the user uses tool, redirects to route "/fight"
+            global itemUsed
+            itemUsed=False
             return redirect("/fight")
         else:#If the event has scavenging (is a store) redirects user to /store route
             if act.values()[0][2]<=ct.values()[0][2]:
@@ -101,15 +103,18 @@ def store():
 @app.route("/fight")
 def fight():
     global ct
-    if act.values()[0][0]>ct.values()[0][0]:
-        global ch
-        dif=(act.values()[0][0]-ct.values()[0][0])*10
-        ch-=dif
-    enmDif=0
-    enmDif=(ct.values()[0][0]-act.values()[0][0])+0.5
-    if enmDif<0.5:
-        enmDif=0.5
-    act.values()[0][3]-=enmDif
+    if itemUsed==False:
+        if act.values()[0][0]>ct.values()[0][0]:
+            global ch
+            dif=(act.values()[0][0]-ct.values()[0][0])*10
+            ch-=dif
+        else:
+            ch-=5:
+        enmDif=0
+        enmDif=(ct.values()[0][0]-act.values()[0][0])+0.5
+        if enmDif<0.5:
+            enmDif=0.5
+        act.values()[0][3]-=enmDif
     if ch<=0:
         return render_template('lose.html')
     elif act.values()[0][3]>0:
